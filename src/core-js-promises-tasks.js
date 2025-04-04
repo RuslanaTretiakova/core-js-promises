74a939d11541d1804583dec95a2fe42e8a6cca97
Ruslana <tretiakovaruslana7@gmail.com>
2025-04-04 13:58:19 +0200

    feat: add solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -39,8 +39,8 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
@@ -56,8 +56,19 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    let rejections = 0;
+
+    promises.forEach((promise) => {
+      promise.then(resolve).catch(() => {
+        rejections += 1;
+        if (rejections === promises.length) {
+          reject(new Error('Promise rejected'));
+        }
+      });
+    });
+  });
 }
 
 /**
